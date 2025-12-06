ec98bc9a55be06443981edd91f9e5fe8352e6af3
kanekos99 <kanekos99@users.noreply.github.com>
2025-12-06 07:10:44 +0000

    deploy: 25c3cb08780046b08954cd4d51ae6fa7740b37ac

--- scripts/main.js
+++ scripts/main.js
@@ -4,5 +4,33 @@ const app = {
   },
 };
 
-app.init();
+const modalImg = document.getElementById("modal-image");
 
+function showNextOrPrevImg(direction) {
+  const visibleImages = Array.from(
+    document.querySelectorAll(".post-body-img-l")
+  );
+  const currentSrc = modalImg.src;
+  let currentIndex = visibleImages.findIndex((img) => img.src === currentSrc);
+  let nextIndex = currentIndex + direction;
+  if (direction === 1 && nextIndex >= visibleImages.length) {
+    nextIndex = 0;
+  } else if (direction === -1 && nextIndex === -1) {
+    nextIndex = visibleImages.length - 1;
+  }
+  showImage(visibleImages[nextIndex]);
+}
+
+function showImage(image) {
+  console.log("Clicked show image!");
+  console.log(image);
+
+  modalImg.style.display = "none";
+  modalImg.src = image.src;
+
+  modalImg.onload = function () {
+    modalImg.style.display = "block";
+  };
+}
+
+app.init();
